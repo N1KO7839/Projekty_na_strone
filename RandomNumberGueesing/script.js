8cdf3306dea22614a8d00988b6312b08245944cb
Filip Jakubiec <140983640+TeeqPRO@users.noreply.github.com>
2024-01-19 11:29:27 +0100

    Add files via upload

---
+++ RandomNumberGueesing/script.js
@@ -0,0 +1,7 @@
+let frominput = document.getElementById("from")
+let toinput = document.getElementById("to")
+
+function updatenumbers(){
+    console.log(frominput.value)
+    console.log(toinput.value)
+}
