5c23d821b9da67c9a0d2cc052fa3ef809df0fe7b
Filip Jakubiec <140983640+TeeqPRO@users.noreply.github.com>
2024-01-19 12:33:24 +0100

    Delete RandomNumberGueesing directory

--- RandomNumberGueesing/script.js
+++
@@ -1,7 +0,0 @@
-let frominput = document.getElementById("from")
-let toinput = document.getElementById("to")
-
-function updatenumbers(){
-    console.log(frominput.value)
-    console.log(toinput.value)
-}
