bdbef0fbfe24542e7042066d9b7ff7a3fb3553c0
Filip Jakubiec <140983640+TeeqPRO@users.noreply.github.com>
2024-01-21 20:46:34 +0100

    Update script.js

--- Hobby strona super/Games/RGN/script.js
+++ Hobby strona super/Games/RGN/script.js
@@ -1,36 +1,67 @@
+let fromtodiv = document.getElementById("fromtodiv");
 let toinput = document.getElementById("to");
+let output = document.getElementById("output");
+let output2 = document.getElementById("stats");
+let livesdiv = document.getElementById("lives");
 let tovalue = 10;
-let guesses = 0;
-let player_lives = 0;
+let player_lives = 5;
 let score = 0;
+let dots = 1;
 let loading = true;
+
+let RandomNumber = Math.floor(Math.random() * tovalue + 1);
 
 function updatenumbers(){
     tovalue = toinput.value
+    RandomNumber = Math.floor(Math.random() * tovalue + 1);
 }
 
-while (loading === true){
-    //interval ze loading czy cos i sie kropki zmieniaja ze np loading... loading.. loading. i to w pentli dopuki loading nie jest true
-};
+setInterval(() => {
+    if(loading === true){
+        if(dots === 1){
+            output.innerHTML = "Loading."
+            dots = 2
+        } else if(dots === 2){
+            output.innerHTML = "Loading.."
+            dots = 3
+        } else if(dots === 3){
+            output.innerHTML = "Loading..."
+            dots = 1
+        }
+    }
+}, 1000);
 
-function Submit(){
-    loading = false;
-    let playerinput = document.getElementById("player-input")
-    let RandomNumber = Math.floor(Math.random() * tovalue+1);
+function Submit() {
+    let playerinput = document.getElementById("player-input");
+    console.log(RandomNumber);
+    console.log(playerinput.value);
 
-    if(playerinput.value)
+    if (playerinput.value) {
+        if (parseFloat(playerinput.value) > tovalue) {
+            playerinput.value = `Max is ${tovalue}!`;
+        } else {
+            loading = false;
+            fromtodiv.style.display = "none";
 
-    if(playerinput.value == RandomNumber){
-        console.log(`Bot ${RandomNumber}`)
-        console.log(`Player ${playerinput.value}`)
-        console.log("Player Wins!")
-    } else if(playerinput.value != RandomNumber){
-        console.log(`Bot ${RandomNumber}`)
-        console.log(`Player ${playerinput.value}`)
-        console.log("Bot Wins!")
-    } else{
-        console.log(`Bot ${RandomNumber}`)
-        console.log(`Player ${playerinput.value}`)
-        console.log("Error")
+            if (player_lives > 0) {
+                if (parseFloat(playerinput.value) == RandomNumber) {
+                    output.innerHTML = "You Won!";
+                    score += 1;
+                }
+                else if (parseFloat(playerinput.value) > RandomNumber) {
+                    output.innerHTML = "Your number is too high!";
+                    player_lives -= 1;
+                    livesdiv.innerHTML = `Lives: ${player_lives}`;
+                }
+                else if (parseFloat(playerinput.value) < RandomNumber) {
+                    output.innerHTML = "Your number is too low!";
+                    player_lives -= 1;
+                    livesdiv.innerHTML = `Lives: ${player_lives}`;
+                }    
+            } else {
+                output.innerHTML = "You Lost!";
+                output2.innerHTML = `Bot number: ${RandomNumber}`;
+            }
+        }
     }
 }
